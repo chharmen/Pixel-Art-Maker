e3b00393fa34e6c60c7f13daf28d04409c231263
chharmen <34026952+chharmen@users.noreply.github.com>
2018-03-02 12:19:10 +0100

    Add files via upload

--- disenio-.js
+++ disenio-.js
@@ -1,16 +1,17 @@
+$(function(){
 function makeGrid() {
       // store values from user
       let gridHeigth = document.getElementById('grid-heigth').value;
-      let gridWidth = $('grid-width').val();  // question: is there any difference between this both?
+      let gridWidth = $('#grid-width').val();    // question: is there any difference between this both?
       let table = document.getElementById('designCanvas');
        // code to create table:
        table.innerHTML = '';
        let tbody = document.createElement('tbody');
-          for (var i = 0; i < gridHeigth; i++) {
-            var tr = document.createElement('tr');
-            for (var h = 0; i < gridWidth; h++) {
-              var td = document.createElement('td');
-      td.appendChild(document.createTextNode('')); //string containing the data to be put in the text node
+          for (let i = 0; i < gridHeigth; i++) {
+            let tr = document.createElement('tr');
+            for (let h = 0; i < gridWidth; h++) {
+              let td = document.createElement('td');
+              td.className = 'grid-cell';
       tr.appendChild(td);
     }
     tbody.appendChild(tr);
@@ -18,7 +19,8 @@ function makeGrid() {
     table.appendChild(tbody);
     }
     // To change the color of cell when clicked:
-    $('body').on.('Click' , 'td' , function() {
-      var color = document.getElementById('color').value;
+    $('body').on('Click' , 'td' , function() {
+      let color = document.getElementById('color').value;
       $(this).css('background-color' , color);
-    });    
+    }
+  });
